37ce7b535b2dc32d4326116fd8549f1ac384574b
Antonin Raffin <antonin.raffin@ensta.org>
2024-08-25 10:08:06 +0200

    Add TD3 hyperparams

---
+++ hyperparams/td3.py
@@ -0,0 +1,29 @@
+default_hyperparams = dict(
+    n_envs=1,
+    n_timesteps=int(1e6),
+    policy="MlpPolicy",
+    qf_learning_rate=1e-3,
+    noise_type="ornstein-uhlenbeck",
+    noise_std=0.1,
+    batch_size=256,
+    # policy_kwargs={},
+    learning_starts=5000,
+    # env_wrapper=[
+    #     # {"rl_zoo3.wrappers.HistoryWrapper": {"horizon": 2}},
+    #     {
+    #         "custom_envs.filter_wrappers.ActionFilterWrapper": {
+    #             "sampling_rate": 60,
+    #             "lowcut": 0,
+    #             "highcut": 4,
+    #         }
+    #     },
+    # ],
+)
+
+hyperparams = {}
+
+for env_id in [
+    "TinyPhysicsEnv-v0",
+    "LatAccel-v0",
+]:
+    hyperparams[env_id] = default_hyperparams
