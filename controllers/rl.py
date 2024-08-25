3e51fa59f21028c50df4641b97b9392e377e4b2c
Antonin Raffin <antonin.raffin@ensta.org>
2024-08-25 12:58:53 +0200

    Update deploy script

--- controllers/rl.py
+++ controllers/rl.py
@@ -1,8 +1,8 @@
+import pickle
 from collections import namedtuple
 
 import numpy as np
 import sbx
-from stable_baselines3.common.vec_env import VecNormalize
 
 from . import BaseController
 
@@ -11,7 +11,7 @@ MAX_LATACCEL = 5
 V_MAX = 50.0
 OBS_FUTURE_PLAN_STEPS = 5
 ACC_G = 9.81
-
+MAX_ERROR_SUM = 10.0
 
 State = namedtuple("State", ["roll_lataccel", "v_ego", "a_ego"])
 FuturePlan = namedtuple("FuturePlan", ["lataccel", "roll_lataccel", "v_ego", "a_ego"])
@@ -21,13 +21,16 @@ class Controller(BaseController):
     def __init__(
         self,
     ):
-        model_path = "./logs/crossq/LatAccel-v0_13/"
-        self.rl_model = sbx.CrossQ.load(f"{model_path}/best_model.zip")
+        model_path = "./logs/ppo/LatAccel-v0_23/"
+        self.rl_model = sbx.PPO.load(f"{model_path}/best_model.zip")
         self.action_scale = 0.5
-        self.vec_norm = VecNormalize.load(f"{model_path}/LatAccel-v0/vecnormalize.pkl")
+        # self.vec_norm = VecNormalize.load(f"{model_path}/LatAccel-v0/vecnormalize.pkl")
+        with open(f"{model_path}/LatAccel-v0/vecnormalize.pkl", "rb") as file_handler:
+            self.vec_norm = pickle.load(file_handler)
+
         self.last_error = 0.0
         self.error_integral = 0.0
-        self.pid_coef = 0.0
+        self.pid_coef = 0.5
 
     @staticmethod
     def pid_action(error: float, error_integral: float, last_error: float) -> float:
@@ -78,16 +81,18 @@ class Controller(BaseController):
     def update(self, target_lataccel: float, current_lataccel: float, state: State, future_plan: FuturePlan) -> float:
         current_error = target_lataccel - current_lataccel
         self.error_integral += current_error
+        self.error_integral = np.clip(self.error_integral, -MAX_ERROR_SUM, MAX_ERROR_SUM)
 
         obs = self.get_observation(state, current_lataccel, target_lataccel, future_plan, self.last_error, self.error_integral)
         norm_obs = self.vec_norm.normalize_obs(obs)
-        action = self.rl_model.predict(norm_obs, deterministic=True)
+        action, _ = self.rl_model.predict(norm_obs, deterministic=True)
+        action *= self.action_scale
 
         pid_action = Controller.pid_action(current_error, self.error_integral, self.last_error)
 
         # Blend PID and RL
         h = self.pid_coef
-        action = h * pid_action + (1.0 - h) * action
+        action = h * pid_action + (1.0 - h) * float(action)
 
         self.last_error = current_error
 
